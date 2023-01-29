aecf8ca3df150d8898788d07f70e7334cd955701
Sergey Basov <basov888@ya.ru>
2023-01-29 18:23:17 +0700

    New project Lesson4 Ex02

---
+++ Ex02/Program.cs
@@ -0,0 +1,12 @@
+﻿// Двумерные массивы 
+
+int [,] matrix = new int[5,4];
+
+for (int rows = 0; rows < matrix.GetLength(0); rows++)
+{
+    for (int columns = 0; columns < matrix.GetLength(1); columns++)
+    {
+        Console.Write($"{matrix[rows, columns]} ");
+    }
+    Console.WriteLine();
+}
