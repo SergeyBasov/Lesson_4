96f407c3edbad542991b94bdadd38e15e23e7f8f
Sergey Basov <basov888@ya.ru>
2023-01-29 18:24:26 +0700

    New project Lesson4 Ex04

---
+++ Ex04/Program.cs
@@ -0,0 +1,44 @@
+﻿// Рекурсия Заполняемые картинки через массив.
+
+int[,] pic = new int[,]
+{
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    {0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 },
+    {0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+    {0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+    {0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+    {0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 },
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+};
+
+void PrintImege(int[,] imege)
+{
+    for (int i = 0; i < imege.GetLength(0); i++)
+    {
+        for (int j = 0; j < imege.GetLength(1); j++)
+        {
+            // Console.Write($"{imege[i,j]} ");
+            if (imege[i, j] == 0) Console.Write($" ");
+            else Console.Write($"+");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillImege(int row, int col)
+{
+    if (pic[row, col] == 0)
+    {
+        pic[row, col] = 1;
+        FillImege(row - 1, col);
+        FillImege(row, col - 1);
+        FillImege(row + 1, col);
+        FillImege(row, col + 1);
+    }
+}
+
+PrintImege(pic);
+FillImege(4, 8);
+PrintImege(pic);
