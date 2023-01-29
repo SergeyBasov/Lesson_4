1b71252b9e2c4864ad146120cd28f917effaa714
Sergey Basov <basov888@ya.ru>
2023-01-29 18:24:46 +0700

    New project Lesson4 Ex05

---
+++ Ex05/Program.cs
@@ -0,0 +1,13 @@
+﻿// Поиск факториала через рекурсию
+
+double Factorial(int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+for (int i = 1; i < 60; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
